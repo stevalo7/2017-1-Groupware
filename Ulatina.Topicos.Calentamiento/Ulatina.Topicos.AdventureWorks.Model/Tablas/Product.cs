34a1d9f783c57482a49c1b88f70f2ebad891176f
laboratorio <laboratorio@LAB09-PROFESOR>
2017-02-18 16:06:07 -0600

    Se agrega proyecto para conexión con AdventureWorks utilizando EntityFramework

---
+++ Ulatina.Topicos.Calentamiento/Ulatina.Topicos.AdventureWorks.Model/Tablas/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ulatina.Topicos.AdventureWorks.Model
+{
+    public partial class Product
+    {
+        public string SizeInInches { get
+            {
+                double laConversion;
+                double lasPulgadas;
+                string elResultado = string.Empty;
+                if (SizeUnitMeasureCode == "CM")
+                {
+                    if (double.TryParse(Size, out laConversion))
+                    {
+                        lasPulgadas = laConversion / 2.54;
+                        elResultado = string.Format("{0} in.", lasPulgadas.ToString());
+                    }
+                    else
+                        elResultado = "error";
+                }
+                else
+                    elResultado = "unidad desconocida";
+                return elResultado;
+            }  }
+    }
+}
